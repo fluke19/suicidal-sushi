6d8920f1d38b773df75cd18e4ec7990747c5c8cf
fluke19 <fluke19@gmail.com>
2015-08-23 22:19:14 +0100

    Updated wp-config.php

--- prototype/EJF-executive-search-wordpress/wp-config.php
+++ prototype/EJF-executive-search-wordpress/wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'fluke19_wp1');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'fluke19_wp1');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Dragonball1');
+define('DB_PASSWORD', 'V#X1&phDNK11.(9');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
