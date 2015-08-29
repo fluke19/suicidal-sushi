b7326b6000f79827c22cabcf02291f9a2b9090d6
fluke19 <fluke19@gmail.com>
2015-08-29 20:57:17 +0100

    testing duplicator
    see if it works

--- prototype/EJF-executive-search-wordpress/searchform.php
+++
@@ -1,7 +0,0 @@
-<form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
-    <div>
-        <input type="text" id="s" name="s" value="" class="form-control" placeholder="Type your search..." />
-        
-        <input type="submit" value="Search" id="searchsubmit" class="hide" />
-    </div>
-</form>
--- prototype/EJF-executive-search-wordpress/wp-content/themes/EJFExecutiveSearchTheme-1/inc/meta.php
+++
@@ -1,7 +0,0 @@
-<div class="meta">
-	<div>Written by <span class="author"><?php the_author_link(); ?></span></div> 
-	<div>/</div>
-	<div><?php the_time('F jS, Y') ?></div>
-	<div>/</div>
-	<div><?php the_tags('', ', '); ?></div>
-</div>
